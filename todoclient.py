9136fae5f9ddc4e430a319a6a834ebf94980c4de
Emiel Kok <emielkok@hotmail.com>
2019-05-15 11:56:32 +0200

    Changed client UI

--- todoclient.py
+++ todoclient.py
@@ -11,12 +11,16 @@ class ClientGui:
         master.title("Client GUI")
 
         self.connect_button = Button(master, text="Connect", command=self.connect)
-        self.connect_button.grid(row=0, column=1)
+        self.connect_button.grid(row=0, column=0)
 
         #e1.grid(row=0, column=1)
         #e2.grid(row=1, column=1)
 
         self.send_button = Button(master, text="Send", command=self.send)
+
+        self.label1 = Label(master, text="What do you want to do?")
+        self.e1 = Entry(master)
+        self.statuslabel = Label(master, text="")
 
         self.s = socket.socket()
 
@@ -24,15 +28,21 @@ class ClientGui:
         print("Greetings!")
         port = 3125
         ip_address = simpledialog.askstring("input string", "Choose IP")
+        connectionstat = "Connected to \n" + ip_address
+        self.statuslabel.configure(text=connectionstat)
         self.s.connect((ip_address, port))
         self.connect_button.destroy()
-        self.send_button.grid(row=0, column=0)
+        self.statuslabel.grid(row=0, column=0)
+        self.label1.grid(row=0, column=1)
+        self.e1.grid(row=1, column=1)
+        self.send_button.grid(row=2, column=1)
 
     def send(self):
-        z = simpledialog.askstring("input string", "What do you want to do?")
-        self.s.sendall(z.encode())
-        #self.s.close()
-        #self.s.shutdown(socket.SHUT_WR)
+        x = self.e1.get()
+        self.s.sendall(x.encode())
+
+        self.e1.delete(0, END)
+
 
     def on_closing(self):
         try:
